f289818b0302fc5a5d585bb0898b4e5a57d9c04f
Mark Chiang <markchiang1@gmail.com>
2018-06-22 06:13:53 -0700

    aritcles appending from mongo db

--- public/js/app.js
+++ public/js/app.js
@@ -5,16 +5,18 @@ $(document).ready(function(){
 
     $(document).on('click', '#download-button', function(){
         console.log('clicked button')
-        $.getJSON('/all/articles', function(err,data){
-            if (err) throw err
-            // console.log(res)
-            var articles = []
-            articles =  data;
+        $.getJSON('/all/articles', function(data, err){
+            if (err) console.log(err);
+            // console.log(data)
+            // var articles = []
+            // articles =  data;
             for (var i=0; i<data.length; i++){
                 // console.log('in the front end loop'+ i)
-                console.log('in the for loop getting articles')
-                $('#articles-append').append('<p>' + data[i].title + '/n' + data[i].link +'/n</p>')
+                // console.log('in the for loop getting articles')
+                // console.log(data[i]);
+                
+                $('#articles-append').append('<ol>' + data[i].title + '</ol><p>' + data[i].link +'</p>')
             }
-        })
+        })  
     })
 })
